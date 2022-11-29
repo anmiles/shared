8688d634c63d511d44bffb6acb406cad65558730
Anatoliy Oblaukhov <mail@anmiles.net>
2022-11-29 18:47:35 +0300

    split * for undoable changes

--- split.ps1
+++ split.ps1
@@ -3,18 +3,19 @@
     Apply only part of patch
 .PARAMETER filename
     Which file to split patch from
-.PARAMETER keep
-    Whether to not move processed file
+.PARAMETER R
+    Whether to work with undo path (if false - work with fix path)
 #>
 
 Param (
     [Parameter(Mandatory = $true)][string]$filename,
-    [switch]$keep
+    [switch]$R
 )
 
 Import-Module $env:MODULES_ROOT\patch.ps1 -Force
 
-$command = "git apply"
+$sh_r = switch($R) { $true {"-R"} $false {""} }
+$command = switch($R) { $true {"git apply -R"} $false {"git apply"} }
 $moveTo = $null
 $result = Patch -filename $filename -command $command -moveTo $moveTo
 
@@ -30,6 +31,10 @@ repo -name this -quiet -action {
     Copy-Item $target $temp
     Read-Host
     $temp_sh = shpath $temp -native -resolve
+    $modified_sh = shpath $modified -native -resolve
     $file_sh = shpath $file -native -resolve
-    sh "git diff $target > $modified; git diff --no-index $target $temp_sh > $file_sh; sed -i 's|b$temp_sh|b/$target|g' $file_sh"
+    $applied_sh = switch($R){ $true {$file_sh} $false {$modified_sh} }
+    $postponed_sh = switch($R){ $true {$modified_sh} $false {$file_sh} }
+    "git diff $sh_r $target > $applied_sh; git diff --no-index $target $temp_sh > $postponed_sh; sed -i 's|b$temp_sh|b/$target|g' $postponed_sh"
+    sh "git diff $sh_r $target > $applied_sh; git diff --no-index $target $temp_sh > $postponed_sh; sed -i 's|b$temp_sh|b/$target|g' $postponed_sh"
 }
