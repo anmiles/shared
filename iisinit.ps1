8838adea4347c7609adce7bc554d13a5ab057c3b
Anatoliy Oblaukhov <mail@anmiles.net>
2023-06-05 10:13:44 +0300

    iisinit * fix output

--- iisinit.ps1
+++ iisinit.ps1
@@ -37,7 +37,7 @@ $iis_config.root.sections.section | % {
                 $urls | ? { $_ } | % {
                     $url = $_.Replace("{0}", $site.prefix)
 
-                    "CreateIISRecord -name $name -url $url -directory $directory -local_ip $template.ip -http $http -https $https -persistent $true -hosts $hosts -hosts_section $hosts_section -port $template.port"
+                    "`nCreateIISRecord -name $name -url $url -directory $directory -local_ip $($template.ip) -http $http -https $https -persistent $true -hosts $hosts -hosts_section $hosts_section -port $($template.port)"
                     CreateIISRecord -name $name -url $url -directory $directory -local_ip $template.ip -http $http -https $https -persistent $true -hosts $hosts -hosts_section $hosts_section -port $template.port
                 }
             }
