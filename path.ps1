8e98b18e970851f388562ed6f2623afdb97bc460
Anatoliy Oblaukhov <mail@anmiles.net>
2023-06-12 09:31:50 +0300

    path * show all paths in PATH environment variable

---
+++ path.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Show all paths in PATH environment variable
+#>
+
+$prev_root = ""
+$prev_dir = ""
+$colors = @(2, 4, 6, 3, 5)
+$root_color_index = 0
+$dir_color = $false
+
+"----------------------------------------"
+$env:PATH -split ";" | Sort | ? { $_ } | % {
+	$root = ($_ -split '\\' | Select -First 2) -join "\"
+	$dir = ($_ -split '\\' | Select -First 3) -join "\"
+
+	if ($prev_root -and $prev_root -ne $root) {
+		$root_color_index ++
+		$dir_color = $false
+	}
+
+	if ($prev_dir -ne $dir) {
+		$dir_color = !$dir_color
+	}
+
+	$prev_root = $root
+	$prev_dir = $dir
+	$root_color = $colors[$root_color_index % $colors.Length]
+	if ($dir_color) { $root_color += 8 }
+
+	Write-Host $_ -ForegroundColor $root_color
+}
+"----------------------------------------"
