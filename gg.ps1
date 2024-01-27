4f23a590faf8d3ac6a93b1f38cdadf598a58c338
Anatoliy Oblaukhov <mail@anmiles.net>
2024-01-28 00:42:02 +0300

    gg * grep specified text in specified files existing in git

---
+++ gg.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+    Grep specified text in specified files existing in git
+.PARAMETER file_pattern
+    Pattern to search the file
+.PARAMETER text_pattern
+    Pattern to search the text in the file
+.EXAMPLE
+    gg '\.ts$' import
+    # search all imports in all *.ts files in current repository
+#>
+
+Param (
+    [string]$file_pattern,
+    [string]$text_pattern
+)
+
+git ls-files | grep $file_pattern | % {
+	$file = $_
+	if (!(Test-Path $file)) {
+		return
+	}
+
+	grep $text_pattern ($file.Replace("/", "\\")) | % { "$file`t$_"}
+}
