133749d7ddca12f7dc3dc746ef4d8aefd06f3ddb
Anatoliy Oblaukhov <mail@anmiles.net>
2024-05-25 15:42:27 +0300

    yadns * fix parametrized domain

--- yadns.ps1
+++ yadns.ps1
@@ -25,7 +25,7 @@ Param (
 		$index = $_
 		if (!($index -eq 1 -and $time -eq 1)) { Start-Sleep -Milliseconds 500 }
 		$dns = "dns$index.yandex.net"
-		$answer = nslookup anmiles.net $dns
+		$answer = nslookup $domain $dns
 		$line = @($answer | grep "Address:" | Select -Skip 1)[0]
 		$address = $line.Replace("Address:", "").Trim()
 		"$dns : $address"
