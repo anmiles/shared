675be4a991972fcb5ec661d48af033afa3733086
Anatoliy Oblaukhov <mail@anmiles.net>
2024-06-09 18:51:04 +0300

    local * unify

--- local.ps1
+++ local.ps1
@@ -2,60 +2,65 @@
 .SYNOPSIS
     Access to resource bypassing current default gateway
 .DESCRIPTION
-    Create persistent route to access specified resources through office gateway
+    Create persistent route to access specified resources through default gateway
 .PARAMETER addresses
     List of ips or dns names or http addresses to route
+.PARAMETER gateway
+    IP address of default gateway
 .EXAMPLE
     local site.com
-    # creates persistent route to access site.com through office gateway
+    # creates persistent route to access site.com through default gateway
 .EXAMPLE
     local http://site2.com/qwerty?asd=wer
-    # creates persistent route to access site.com through office gateway
+    # creates persistent route to access site.com through default gateway
 .EXAMPLE
     local 1.2.3.4
-    # creates persistent route to access 1.2.3.4 through office gateway
+    # creates persistent route to access 1.2.3.4 through default gateway
 .EXAMPLE
     blocked 11.12.13.14 15.16.17.18 example.com
-    # creates persistent route to access each of [11.12.13.14, 15.16.17.18, example.com] through office gateway
+    # creates persistent route to access each of [11.12.13.14, 15.16.17.18, example.com] through default gateway
 #>
 
 Param (
+    [string]$gateway_ip,
     [Parameter(ValueFromRemainingArguments = $true)][string[]]$addresses
 )
 
-$original_default_gateway = "192.168.0.3"
-
 Function AddRoutes($address) {
     $cidrs = @()
-    
+
     if ($address -match "^([\d\.]+)\/\d+$") {
         $cidrs += "$address"
-    } else {    
+    } else {
         if ($address -match "^([\d\.]+)$") {
             $cidrs += "$address/32"
         } else {
             $address = $address -replace "^(\w+://)?([^/]+).*?$", '$2'
-            
+
             [System.Net.Dns]::GetHostAddresses($address) | % {
                 $cidrs += "$($_.IPAddressToString)/32"
             }
         }
     }
-    
+
     $cidrs | % {
         $parts = $_.Split('/');
         $subnet_ip = [System.Net.IPAddress]::Parse($parts[0])
         $subnet_bits = [int]$parts[1]
         $int64 = [convert]::ToInt64(('1' * $subnet_bits + '0' * (32 - $subnet_bits)), 2)
         $subnet_mask = '{0}.{1}.{2}.{3}' -f
-            ([math]::Truncate($Int64 / [Math]::Pow(2, 24))).ToString(), 
-            ([math]::Truncate(($Int64 % [Math]::Pow(2, 24)) / [Math]::Pow(2, 16))).ToString(), 
-            ([math]::Truncate(($Int64 % [Math]::Pow(2, 16))/[Math]::Pow(2, 8))).ToString(), 
+            ([math]::Truncate($Int64 / [Math]::Pow(2, 24))).ToString(),
+            ([math]::Truncate(($Int64 % [Math]::Pow(2, 24)) / [Math]::Pow(2, 16))).ToString(),
+            ([math]::Truncate(($Int64 % [Math]::Pow(2, 16))/[Math]::Pow(2, 8))).ToString(),
             ([math]::Truncate($Int64 % [Math]::Pow(2, 8))).ToString()
-        $subnet_ip.IPAddressToString
-        $subnet_mask
-        route delete $subnet_ip.IPAddressToString
-        route add $subnet_ip.IPAddressToString mask $subnet_mask $original_default_gateway -p
+
+        @(
+            "route delete $($subnet_ip.IPAddressToString)",
+            "route add $($subnet_ip.IPAddressToString) mask $subnet_mask $gateway_ip -p"
+        ) | % {
+            out "> $_" Yellow
+            iex $_
+        }
     }
 }
 
