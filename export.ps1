a7050afc8e01369f79011f6ac4aa0e8e8c7ca55f
Anatoliy Oblaukhov <mail@anmiles.net>
2025-01-10 23:03:38 +0300

    export * executing command in one line * do not persist variables

--- export.ps1
+++ export.ps1
@@ -2,29 +2,37 @@
 .SYNOPSIS
     Creates process-wide environment variable
 .EXAMPLE
-	export.ps1 VAR=value
+	export.ps1 VAR1=value1 VAR2=value2 npm run command
 #>
 
-Param (
-    [Parameter(Mandatory = $true)][string]$expression
-)
+$cmd = @()
+$envars = @()
+$new_envars = @()
+$existing_envars = $env:ENVARS -split ","
+$existing_variables = [Environment]::GetEnvironmentVariables()
 
-$match = [Regex]::Match($expression, '^([^=]+)\s*=\s*(.*?)\s*$')
+$args | % {
+	$match = [Regex]::Match($_, '^([A-Z0-9_]+)\s*=\s*(.*?)\s*$')
 
-if (!$match.Success) {
-	throw "Expected format: export VAR=value"
+	if ($match.Success) {
+		$name = $match.Groups[1].Value
+		$value = $match.Groups[2].Value
+		[Environment]::SetEnvironmentVariable($name, $value, "Process")
+		$new_envars += @($name)
+	} else {
+		$cmd += $_
+	}
 }
 
-$name = $match.Groups[1]
-$value = $match.Groups[2]
-
-[Environment]::SetEnvironmentVariable($name, $value, "Process")
-
-$envars = $env:ENVARS -split ","
-$envars = $envars | ? { $_ -ne $name }
-
-if ($value.Length -gt 0) {
-	$envars += @($name)
-}
+$envars = ($existing_envars + $new_envars) | Get-Unique
 
 [Environment]::SetEnvironmentVariable("ENVARS", ($envars -join ","), "Process")
+
+sh ($cmd -join " ")
+
+[Environment]::SetEnvironmentVariable("ENVARS", ($existing_envars -join ","), "Process")
+
+$new_envars | % {
+	$name = $_
+	[Environment]::SetEnvironmentVariable($name, ($existing_variables | ? { $_.Name -eq $name }).Value, "Process")
+}
