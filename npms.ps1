713dc49a68ff690cb2659a2b4434fb9d2d6fa58b
Anatoliy Oblaukhov <mail@anmiles.net>
2025-03-15 18:26:10 +0300

    npms * fix terminal splitting

--- npms.ps1
+++ npms.ps1
@@ -10,9 +10,9 @@ Param (
 )
 
 $commands = @(
-	@{ name = "build"; console= "s1T50H"; },
-	@{ name = "lint";  console= "s1T75V"; },
-	@{ name = "test";  console= "s2T75V"; }
+	@{ name = "test"; console= "s1T33H"; },
+	@{ name = "build";  console= "s1T50H"; },
+	@{ name = "lint";  console= "s50V"; }
 )
 
 $commands| % {
@@ -20,12 +20,10 @@ $commands| % {
 		Write-Host `"$($_.name.ToUpper())`n--------------------------------`" -ForegroundColor Yellow `
 		$env:GIT_ROOT\env.ps1; `
 		npm run $($_.name); `
-		if (`$LastExitCode -eq 0) { `
-			Write-Host `"--------------------------------`n$($_.name.ToUpper()) PASSED`" -ForegroundColor Green `
-			if (`$noExit) { Read-Host } `
-		} else { `
-			Write-Host `"--------------------------------`n$($_.name.ToUpper()) FAILED`" -ForegroundColor Red `
-			Read-Host `
-		} `
-	} -new_console:$($_.console):n:t:$($_.name)"
+		`$result = `$LastExitCode -eq 0
+		`$resultText = if (`$result) { `"PASSED`" } else { `"FAILED`" }
+		`$resultColor = if (`$result) { `"Green`" } else { `"Red`" }
+		Write-Host `"--------------------------------`n$($_.name.ToUpper()) `$resultText`" -ForegroundColor `$resultColor`
+		Read-Host
+	} $( if ($_.console) { "-new_console:$($_.console):n:t:$($_.name)" })"
 }
